fb3a94111d92db47c5cff2f144b44b0d6755b00f
Bente Schopman <b.l.schopman@gmail.com>
2022-03-08 19:03:35 +0100

    Independent variable encoding country, section 3, lesson 22

--- Part 1 - Data Preprocessing/Python/data_processing.py
+++ Part 1 - Data Preprocessing/Python/data_processing.py
@@ -14,8 +14,9 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
-
+print("Independent variables")
 print(x)
+print("Dependent variabele")
 print(y)
 
 #taking care of missing data
@@ -23,4 +24,15 @@ from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
 imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
+print("Missing data is gome")
+print(x)
+
+#Encoding categorical data
+#Encoding the independemt variabele
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing  import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+print("Encoding the country")
 print(x)
