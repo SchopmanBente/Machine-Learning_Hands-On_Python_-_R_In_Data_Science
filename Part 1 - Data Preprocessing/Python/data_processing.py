562b118114cba0be89a460ed10111619d15bb4a6
Bente Schopman <b.l.schopman@gmail.com>
2022-03-08 18:30:50 +0100

    Section 3, lesson 21

--- Part 1 - Data Preprocessing/Python/data_processing.py
+++ Part 1 - Data Preprocessing/Python/data_processing.py
@@ -9,6 +9,7 @@ Created on Mon Mar  7 15:09:43 2022
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
 #Importing dataset
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:,:-1].values
@@ -17,4 +18,9 @@ y = dataset.iloc[:,-1].values
 print(x)
 print(y)
 
-
+#taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+imputer.fit(x[:,1:3])
+x[:,1:3] = imputer.transform(x[:,1:3])
+print(x)
